878dfa6a67e2c6af92bc0f93a9fe1308643f19ac
Ivo van der Wijk <github@in.m3r.nl>
2023-12-03 17:26:18 +0100

    day 3 complete

--- day3/day3.py
+++ day3/day3.py
@@ -12,6 +12,22 @@ class Engine:
             for line in f.readlines():
                 self.matrix.append(list(line.strip()))
 
+    def collect(self):
+        numbers = []
+        machineparts = []
+
+        for i, row in enumerate(self.matrix):
+            parts = re.findall(r'([^\d]{1,}|\d+)', "".join(row))
+            offset = 0
+            for p in parts:
+                if p[0].isdigit():
+                    numbers.append((i, offset, p))
+                offset += len(p)
+            for j, col in enumerate(row):
+                if not col.isdigit() and col != ".":
+                    machineparts.append((i, j, col))
+        return numbers, machineparts 
+
     def __str__(self):
         return "\n".join("".join(c for c in row) for row in self.matrix)
 
@@ -19,27 +35,8 @@ def part1(filename):
     e = Engine()
     e.read(filename)
 
-    # Alternatively, search for symbols and "clear" nearby numbers
+    numbers, machineparts = e.collect() 
 
-    # or: find the numbers, store their position and length
-    # then find part locations
-    # lastly, check all numbers and see if there's a part nearby
-
-    numbers = []
-    machineparts = []
-
-    for i, row in enumerate(e.matrix):
-        parts = re.findall(r'([^\d]{1,}|\d+)', "".join(row))
-        offset = 0
-        for p in parts:
-            if p[0].isdigit():
-                numbers.append((i, offset, p))
-            offset += len(p)
-        for j, col in enumerate(row):
-            if not col.isdigit() and col != ".":
-                machineparts.append((i, j, col))
-    
-    # find all numbers that are not "near" a part
     s = 0
     def near(nr, nc, nv):
         for pr, pc, pl in machineparts:
@@ -57,7 +54,6 @@ def part1(filename):
         return False
 
     for nr, nc, nv in numbers:
-        # breakpoint()
         if not near(nr, nc, nv):
             print("Not:", nv)
         else:
@@ -66,7 +62,57 @@ def part1(filename):
     print(s)
 
 def part2(filename):
-    pass
+    e = Engine()
+    e.read(filename)
+
+    numbers, machineparts = e.collect() 
+
+    s = 0
+    def near(nr, nc, nv):
+        for pr, pc, pl in machineparts:
+            if abs(pr - nr) <= 1:
+                # start on or 1 after
+                if 0 <= nc - pc <= 1:
+                    return True
+                # end on or 1 before
+                if 0 <= pc - (nc + len(nv) - 1) <= 1:
+                    return True
+                # overlap
+                if nc <= pc <= nc + len(nv) - 1:
+                    return True
+
+        return False
+
+    def near2(pr, pc, pt):
+        nearby = []
+        for nr, nc, nv in numbers:
+            if abs(pr - nr) <= 1:
+                # start on or 1 after
+                if 0 <= nc - pc <= 1:
+                    nearby.append(int(nv))
+                    continue
+                # end on or 1 before
+                if 0 <= pc - (nc + len(nv) - 1) <= 1:
+                    nearby.append(int(nv))
+                    continue
+                # overlap
+                if nc <= pc <= nc + len(nv) - 1:
+                    nearby.append(int(nv))
+                    continue
+        if len(nearby) == 2:
+            return nearby[0] * nearby[1]
+        return 0
+
+    s = 0
+
+    for pr, pc, pt in machineparts:
+        if pt != "*":
+            continue
+
+        if v := near2(pr, pc, pt):
+            s += v
+
+    print(s)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
