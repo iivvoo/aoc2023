73613a176949168245f3c5c55ed31c1cc2f4815e
Ivo van der Wijk <github2023prolap@in.m3r.nl>
2023-12-07 15:42:11 +0100

    make it a one-liner

--- day7/day7.py
+++ day7/day7.py
@@ -155,11 +155,33 @@ def part2(filename: str) -> None:
     print(sum(b.bid * v for v, b in enumerate(bids, 1)))
 
 
+def part2_obscure(filename: str) -> None:
+    print(
+        sum(
+            b.bid * v
+            for v, b in enumerate(
+                sorted(
+                    Bid(
+                        Hand([Card(l, WithJoker=True) for l in labels], WithJoker=True),
+                        int(bid),
+                    )
+                    for labels, bid in [
+                        line.split() for line in open(filename, "r").readlines()
+                    ]
+                ),
+                1,
+            )
+        )
+    )
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print(f"Usage: python {sys.argv[0]} [1|2] <input_file>")
         sys.exit(1)
     if sys.argv[1] == "1":
         part1(sys.argv[2])
-    else:
+    elif sys.argv[1] == "2":
         part2(sys.argv[2])
+    else:
+        part2_obscure(sys.argv[2])
