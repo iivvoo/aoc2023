9448000bcce1c3702bf8c63314b6bbd0e2225c41
Ivo van der Wijk <github@in.m3r.nl>
2023-12-09 12:03:58 +0100

    day9 pt2 solved

--- day9/day9.py
+++ day9/day9.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
 import sys
+from functools import reduce
 
 
 def predict(values: list[int]) -> int:
     p = values[-1]
     while values.count(values[0]) != len(values):
-        values = [b-a for a, b in zip(values, values[1:])]
+        values = [b - a for a, b in zip(values, values[1:])]
         p += values[-1]
     return p
+
+
+def predict2(values: list[int]) -> int:
+    befores = [values[0]]
+
+    while values.count(values[0]) != len(values):
+        values = [b - a for a, b in zip(values, values[1:])]
+        befores.insert(0, values[0])
+
+    v = reduce(lambda a, b: b - a, befores)
+    return v
+
 
 def part1(filename: str) -> None:
     s = 0
@@ -17,8 +30,13 @@ def part1(filename: str) -> None:
         s += predict(values)
     print(s)
 
+
 def part2(filename: str) -> None:
-    pass
+    s = 0
+    for line in open(filename, "r"):
+        values = [int(v) for v in line.split()]
+        s += predict2(values)
+    print(s)
 
 
 if __name__ == "__main__":
