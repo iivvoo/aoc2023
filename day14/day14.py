5c30e75a65be31bf10a21e714b1170204e43bdc3
Ivo van der Wijk <github2023prolap@in.m3r.nl>
2023-12-14 19:20:07 +0100

    day14 wip

--- day14/day14.py
+++ day14/day14.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+from enum import Enum
+
+class Direction(Enum):
+    NORTH = 0
+    SOUTH = 1
+    EAST = 2
+    WEST = 3
 
 
 class Dish:
@@ -10,16 +17,27 @@ class Dish:
     def add(self, row: str) -> None:
         self.rows.append(list(row))
 
-    def tilt(self) -> None:
-        for i in range(1, len(self.rows)):
-            for j, c in enumerate(self.rows[i]):
-                if c != "O":
-                    continue
-                k = i
-                while k > 0 and self.rows[k - 1][j] == ".":
-                    self.rows[k - 1][j] = "O"
-                    self.rows[k][j] = "."
-                    k -= 1
+
+    def tilt_line(self, line, reverse=False) -> str:
+        if reverse:
+            return "#".join("."*x.count(".") + "O"*x.count("O") for x in "".join(line).split("#"))
+        return "#".join("O"*x.count("O") + "."*x.count(".") for x in "".join(line).split("#"))
+
+    def tilt(self, direction: Direction) -> None:
+        if direction in [Direction.NORTH, Direction.SOUTH]:
+            reverse = direction == Direction.SOUTH
+
+            for c in range(len(self.rows[0])):
+                before = [r[c] for r in self.rows]
+                titled = self.tilt_line(before, reverse)
+                                        
+                for i, t in enumerate(titled):
+                    self.rows[i][c] = t
+        else:
+            reverse = direction == Direction.WEST
+            for i, r in enumerate(self.rows):
+                self.rows[i] = list(self.tilt_line(r, reverse))
+
 
     def score(self) -> int:
         return sum(r.count("O") * (len(self.rows) - i) for i, r in enumerate(self.rows))
@@ -34,12 +52,36 @@ def part1(filename: str) -> None:
     for line in open(filename, "r"):
         d.add(line.strip())
 
-    d.tilt()
+    d.tilt(direction=Direction.NORTH)
     print(d.score())
 
 
 def part2(filename: str) -> None:
-    pass
+    d = Dish()
+
+    for line in open(filename, "r"):
+        d.add(line.strip())
+
+    # find when repetition happens
+    # 
+    values = []
+    def isrepeating(v):
+        for i in range(100, len(v) - 100):
+            print(len(v[1:101]), len(v[i+1:i+101]))
+            if v[1:101] == v[i+1:i+101]:
+                return i
+        return -1
+
+    for i in range(5000):
+        d.tilt(direction=Direction.NORTH)
+        d.tilt(direction=Direction.EAST)
+        d.tilt(direction=Direction.SOUTH)
+        d.tilt(direction=Direction.WEST)
+        values.append(d.score())
+
+        if r := isrepeating(values) != -1:
+            print("Repeating at ", r)
+            break
 
 
 if __name__ == "__main__":
