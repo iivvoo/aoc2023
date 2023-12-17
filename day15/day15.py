a95d5a914dcba5949f34e61d041b6d1ae5193c7f
Ivo van der Wijk <github@in.m3r.nl>
2023-12-17 17:47:11 +0100

    day 15 pt 2 solved

--- day15/day15.py
+++ day15/day15.py
@@ -2,6 +2,28 @@
 
 import sys
 
+
+class Box:
+    def __init__(self, no: int):
+        self.no = no
+        self.lenses: dict[bytes, int] = {}
+
+    def add(self, label: bytes, lens: int) -> None:
+        self.lenses[label] = lens
+
+    def remove(self, label: bytes) -> None:
+        try:
+            del self.lenses[label]
+        except KeyError:
+            pass
+
+    def focuspower(self) -> int:
+        return sum(
+            (self.no + 1) * (pos * focal)
+            for pos, focal in enumerate(self.lenses.values(), 1)
+        )
+
+
 def hash(s: bytes) -> int:
     v = 0
     for c in s:
@@ -9,7 +31,8 @@ def hash(s: bytes) -> int:
         v *= 17
         v %= 256
     return v
-        
+
+
 def part1(filename: str) -> None:
     input = open(filename, "rb").readline().strip()
     parts = input.split(b",")
@@ -18,7 +41,20 @@ def part1(filename: str) -> None:
 
 
 def part2(filename: str) -> None:
-    pass
+    input = open(filename, "rb").readline().strip()
+    parts = input.split(b",")
+
+    boxes = [Box(i) for i in range(256)]
+
+    for p in parts:
+        if b"-" in p:
+            label = p[:-1]
+            boxes[hash(label)].remove(label)
+        else:
+            label, lens = p.split(b"=")
+            boxes[hash(label)].add(label, int(lens))
+
+    print(sum(b.focuspower() for b in boxes))
 
 
 if __name__ == "__main__":
