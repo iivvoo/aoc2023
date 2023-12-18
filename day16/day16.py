c6c941c6eca16ca7d71a0e85504575bf646d881e
Ivo van der Wijk <github@in.m3r.nl>
2023-12-18 16:32:23 +0100

    day 16 pt 1 solved

--- day16/day16.py
+++ day16/day16.py
@@ -1,10 +1,228 @@
 #!/usr/bin/env python3
 
 import sys
+from dataclasses import dataclass, field
+from functools import reduce
+
+
+@dataclass(frozen=True)
+class Position:
+    r: int
+    c: int
+
+
+@dataclass(frozen=True)
+class Step:
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+
+    position: Position
+    direction: int
+
+
+class Beam:
+    ID = 0
+
+    direction: int = Step.RIGHT
+    steps: list[Step] = field(default_factory=list)
+    dead: bool = False
+
+    def __init__(self, direction: int, steps: list[Step], dead=False):
+        self.direction = direction
+        self.steps = steps
+        self.dead = dead
+
+        self.id = id
+        self.ID += 1
+
+    def __hash__(self) -> int:
+        return hash(self.id)
+
+    def advance(self, max_rows: int, max_cols: int):
+        if self.dead:
+            return
+
+        last_step = self.steps[-1]
+        last_pos = last_step.position
+
+        next_pos = None
+
+        if self.direction == Step.UP and last_pos.r > 0:
+            next_pos = Position(last_pos.r - 1, last_pos.c)
+        elif self.direction == Step.DOWN and last_pos.r < max_rows - 1:
+            next_pos = Position(last_pos.r + 1, last_pos.c)
+        elif self.direction == Step.LEFT and last_pos.c > 0:
+            next_pos = Position(last_pos.r, last_pos.c - 1)
+        elif self.direction == Step.RIGHT and last_pos.c < max_cols - 1:
+            next_pos = Position(last_pos.r, last_pos.c + 1)
+        else:
+            self.dead = True
+
+        if next_pos is not None:
+            next_step = Step(next_pos, self.direction)
+            if next_step in self.steps:
+                # print("Beam died because of loop")
+                self.dead = True
+            else:
+                self.steps.append(next_step)
+
+    def clone(self) -> "Beam":
+        return Beam(self.direction, self.steps.copy(), self.dead)
+
+
+@dataclass(frozen=True)
+class Splitter:
+    # direction: h / v
+    HORIZONTAL = 1
+    VERTICAL = 2
+
+    position: Position
+    direction: int
+
+    def apply(self, beam: Beam) -> list[Beam]:
+        if self.direction == Splitter.HORIZONTAL:
+            if beam.direction in (Step.UP, Step.DOWN):
+                left = beam.clone()
+                left.direction = Step.LEFT
+                beam.direction = Step.RIGHT
+                return [left, beam]
+        else:  # vertical
+            if beam.direction in (Step.LEFT, Step.RIGHT):
+                down = beam.clone()
+                down.direction = Step.DOWN
+                beam.direction = Step.UP
+                return [beam, down]
+
+        return [beam]  # it passes through
+
+
+@dataclass(frozen=True)
+class Mirror:
+    # direction: / or \
+    FORWARD = 1
+    BACKWARD = 2
+
+    position: Position
+    direction: int
+
+    def apply(self, beam: Beam) -> Beam:
+        if self.direction == Mirror.FORWARD:  # /
+            if beam.direction == Step.RIGHT:
+                beam.direction = Step.UP
+            elif beam.direction == Step.LEFT:
+                beam.direction = Step.DOWN
+            elif beam.direction == Step.UP:
+                beam.direction = Step.RIGHT
+            elif beam.direction == Step.DOWN:
+                beam.direction = Step.LEFT
+        else:  # \
+            if beam.direction == Step.RIGHT:
+                beam.direction = Step.DOWN
+            elif beam.direction == Step.LEFT:
+                beam.direction = Step.UP
+            elif beam.direction == Step.UP:
+                beam.direction = Step.LEFT
+            elif beam.direction == Step.DOWN:
+                beam.direction = Step.RIGHT
+
+        return beam
+
+
+class Empty(Position):
+    pass
+
+
+class Contraption:
+    def __init__(self):
+        self.mirrors: dict[Position, Mirror] = {}
+        self.splitters: dict[Position, Splitter] = {}
+        self.rows: list[str] = []
+
+    def add(self, row: str) -> None:
+        self.rows.append(row)
+        for i, c in enumerate(row):
+            p = Position(len(self.rows) - 1, i)
+            if c == "/":
+                self.mirrors[p] = Mirror(p, Mirror.FORWARD)
+            if c == "\\":
+                self.mirrors[p] = Mirror(p, Mirror.BACKWARD)
+            if c == "-":
+                self.splitters[p] = Splitter(p, Splitter.HORIZONTAL)
+            if c == "|":
+                self.splitters[p] = Splitter(p, Splitter.VERTICAL)
+
+    def all_energized(self, beams: set[Beam]) -> set[Position]:
+        return reduce(lambda a, b: a | set(s.position for s in b.steps), beams, set())
+
+    def run(self) -> None:
+        beams = {Beam(Step.RIGHT, [Step(Position(0, 0), Step.RIGHT)])}
+
+        loops = 0
+
+        while not all(b.dead for b in beams):
+            # breakpoint()
+            e = len(self.all_energized(beams))
+            # if e == 11:
+            #     breakpoint()
+
+            active = set()
+
+            for beam in beams:
+                """
+
+                a beam can die if _any_ step exists
+                """
+                if beam.dead:
+                    # active.add(beam)
+                    continue
+
+                # breakpoint()
+                if m := self.mirrors.get(beam.steps[-1].position):
+                    active.add(m.apply(beam))
+                elif s := self.splitters.get(beam.steps[-1].position):
+                    active.update(s.apply(beam))
+                else:
+                    active.add(beam)
+
+            all_steps = reduce(lambda a, b: a | set(b.steps), beams, set())
+            for b in active:
+                b.advance(len(self.rows), len(self.rows[0]))
+                if b.steps[-1] in all_steps:
+                    b.dead = True
+
+            beams |= active
+            loops += 1
+            if loops % 100 == 0:
+                self.print(beams)
+
+        print(len(self.all_energized(beams)))
+
+    def print(self, beams):
+        all_energized = self.all_energized(beams)
+
+        print("-" * 20)
+        for i, row in enumerate(self.rows):
+            for j, c in enumerate(row):
+                if Position(i, j) in all_energized:
+                    print("#", end="")
+                else:
+                    print(c, end="")
+            print()
+        print("-" * 10 + str(len(all_energized)) + "-" * 10)
+
+    def __str__(self) -> str:
+        return "\n".join(self.rows)
 
 
 def part1(filename: str) -> None:
-    pass
+    c = Contraption()
+    for row in open(filename, "r"):
+        c.add(row.strip())
+
+    print(c)
+    c.run()
 
 
 def part2(filename: str) -> None:
